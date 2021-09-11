fa0b2cfb91a2cb882d24047ecbdf70c7efb71c1b
perio29 <tokuzou10yohji@gmail.com>
2021-09-11 17:47:34 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [todos, setTodos] = useState(["java"]);
+  const [inputText, setInputText] = useState("");
+
+  const onChangeText = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onClickAdd = () => {
+    if (inputText === "") return;
+    const newTodos = [...todos, inputText];
+    setTodos(newTodos);
+    setInputText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo}>
+              <Sli>
+                {todo}
+                <button>編集</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
+              </Sli>
+            </div>
+          );
+        })}
+      </ul>
+      <input
+        placeholder="何をしようかな"
+        value={inputText}
+        onChange={onChangeText}
+      />
+      <button onClick={onClickAdd} disabled={!inputText}>
+        保存
+      </button>
     </div>
   );
 }
+
+const Sli = styled.li`
+  list-style-type: decimal;
+`;
 
 export default App;
