717e97ba6fdab9f736abaca529069adf581f4e40
perio29 <tokuzou10yohji@gmail.com>
2021-09-14 17:27:23 +0900

    📝 編集機能を追加

--- src/App.js
+++ src/App.js
@@ -4,46 +4,78 @@ import styled from "styled-components";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [inputText, setInputText] = useState("");
 
-  const onChangeText = (e) => {
-    setInputText(e.target.value);
+  const [newTodos, setNewTodos] = useState("");
+
+  const saveNewTodo = () => {
+    setTodos([...todos, newTodos]);
+    setNewTodos();
   };
 
-  const onClickAdd = () => {
-    if (inputText === "") return;
-    const newTodos = [...todos, inputText];
-    setTodos(newTodos);
-    setInputText("");
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  const onClickDelete = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+  const changeTodo = (id, text) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, text };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const toggleEditing = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isEditing: !todo.isEditing };
+        }
+        return todo;
+      })
+    );
   };
 
   return (
     <div className="App">
-      <ul>
-        {todos.map((todo, index) => {
-          return (
-            <div key={todo}>
-              <Sli>
-                {todo}
-                <button>編集</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </Sli>
-            </div>
-          );
-        })}
-      </ul>
+      <ol>
+        {todos.map((todo) => (
+          <Sli key={todo.id}>
+            {!todo.isEditing ? (
+              todo.text
+            ) : (
+              <input
+                value={todo.text}
+                onChange={(e) => changeTodo(todo.id, e.target.value)}
+              />
+            )}
+            {!todo.isEditing && (
+              <button onClick={() => toggleEditing(todo.id)}>編集</button>
+            )}
+            <button onClick={() => deleteTodo(todo.id)}>削除</button>
+            {todo.isEditing && (
+              <button onClick={() => toggleEditing(todo.id)}>完了</button>
+            )}
+          </Sli>
+        ))}
+      </ol>
       <input
         placeholder="何をしようかな"
-        value={inputText}
-        onChange={onChangeText}
+        value={newTodos ? newTodos.text : ""}
+        onChange={(e) => {
+          setNewTodos({
+            text: e.target.value,
+            id: Date(),
+            isEditing: false,
+          });
+          if (!e.target.value) {
+            setNewTodos();
+          }
+        }}
       />
-      <button onClick={onClickAdd} disabled={!inputText}>
+      <button onClick={saveNewTodo} disabled={!newTodos}>
         保存
       </button>
     </div>
